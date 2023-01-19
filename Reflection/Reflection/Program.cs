1a59aa3f5d13df23573c7183ae37286a79952614
Rafael G <rafael.gualdieri.pinheiro@gmail.com>
2023-01-18 21:58:09 -0300

    Exercicio

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student.Name = "Rafael";
+            student.University = "FIAP";
+            student.RollNumber = 0;
+
+            DisplayPublicProperties(student);
+
+            Console.WriteLine();
+
+            Student student1 = CreateInstance();
+
+            MethodInfo methodInfo = student1.GetType().GetMethod("DisplayInfo");
+            methodInfo.Invoke(student1, null);
+        }
+
+        public static void DisplayPublicProperties(Student student)
+        {
+            PropertyInfo[] propriedades = student.GetType().GetProperties();
+
+            foreach (PropertyInfo propriedade in propriedades)
+            {
+                Console.WriteLine($"{propriedade.Name}: {propriedade.GetValue(student)}");
+            }
+        }
+
+        public static Student CreateInstance()
+        {
+            Student student1 = new Student();
+
+            student1.GetType().GetProperty("Name").SetValue(student1, "Leafar");
+            student1.GetType().GetProperty("University").SetValue(student1, "UFABC");
+            student1.GetType().GetProperty("RollNumber").SetValue(student1, 1);
+
+            return student1;
+        }
+    }
+}
